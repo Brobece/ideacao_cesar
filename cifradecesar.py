21308de5657a5784b0b71eb63cd838ae96e3cdcb
Brobece <74504989+Brobece@users.noreply.github.com>
2025-04-02 15:58:31 -0300

    Update cifradecesar.py

--- cifradecesar.py
+++ cifradecesar.py
@@ -1,1 +1,6 @@
+instrucao = str(input("Qual texto você deseja criptografar?"))
+print(instrucaoaa)
+
+letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+print(letras[5])
 
